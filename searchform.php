2cb0878bd7c9548fc75a4920b552760eb3a42a30
art2web <contato@art2web.com.br>
2014-03-12 18:37:53 -0300

    Create searchform.php

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '' ) ); ?>/">
+    <div>
+        <input type="text" value="" placeholder="BUSCAR PRODUTO" style="color: #ffffff;" name="s" id="s" />
+        <input type="submit" id="searchsubmit" value="" />
+    </div>
+</form>
