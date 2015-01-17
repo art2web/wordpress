1b0e578e386809f0636ec843ce2d64ff51bfb1b5
Daniel Ferraz Ramos <contato@art2web.com.br>
2015-01-17 03:37:45 -0200

    Create common-functions.php

---
+++ common-functions.php
@@ -0,0 +1,28 @@
+<?php
+/* =================================================================================================== */
+/** Admin Slug Function */
+function the_slug() {
+    $post_data = get_post($post->ID, ARRAY_A);
+    $slug = $post_data['post_name'];
+    return $slug; 
+}
+/* =================================================================================================== */
+
+add_action('init', 'cloneRole');
+
+function cloneRole()
+{
+    global $wp_roles;
+    if ( ! isset( $wp_roles ) )
+        $wp_roles = new WP_Roles();
+
+    $adm = $wp_roles->get_role('administrator');
+    $aut = $wp_roles->get_role('administrator');
+    //Adding a 'new_role' with all admin caps
+    $wp_roles->add_role('agencia', 'Agência', $adm->capabilities);
+    $wp_roles->add_role('cliente', 'Cliente', $aut->capabilities);
+}
+
+/* =================================================================================================== */
+
+?>
