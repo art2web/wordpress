99aed6b5604a276c0e78bbea08f6e5396fd07ecd
Daniel Ferraz Ramos <contato@art2web.com.br>
2015-01-21 10:29:10 -0200

    Update common-functions.php

--- common-functions.php
+++ common-functions.php
@@ -9,18 +9,20 @@ function the_slug() {
 /* =================================================================================================== */
 
 add_action('init', 'cloneRole');
-
 function cloneRole()
 {
     global $wp_roles;
     if ( ! isset( $wp_roles ) )
         $wp_roles = new WP_Roles();
-
     $adm = $wp_roles->get_role('administrator');
     $aut = $wp_roles->get_role('administrator');
     //Adding a 'new_role' with all admin caps
     $wp_roles->add_role('agencia', 'Agência', $adm->capabilities);
     $wp_roles->add_role('cliente', 'Cliente', $aut->capabilities);
+    $wp_roles->remove_role('author', 'Author', $aut->capabilities);
+    $wp_roles->remove_role('colaborador', 'Colaborador', $aut->capabilities);
+    $wp_roles->remove_role('assinante', 'Assinante', $aut->capabilities);
+    $wp_roles->remove_role('editor', 'Editor', $aut->capabilities);
 }
 
 /* =================================================================================================== */
