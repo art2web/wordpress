bdd05889bb4ac1beaef221853243bd71e993749d
Daniel Ferraz Ramos <contato@art2web.com.br>
2015-03-20 20:07:24 -0300

    Update common-functions.php

--- common-functions.php
+++ common-functions.php
@@ -2,6 +2,7 @@
 /* =================================================================================================== */
 /** Admin Slug Function */
 function the_slug() {
+    global $post;
     $post_data = get_post($post->ID, ARRAY_A);
     $slug = $post_data['post_name'];
     return $slug; 
